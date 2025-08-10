a873a7c011f2116dd0ef6c0296c3c974394da2ac
Angelo Ortiz Vega <angelo2898@gmail.com>
2025-08-09 20:45:57 -0600

    feat(ui): add sidenav component; fix notes display

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,21 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { SharedService } from '../../services/shared.service';
+import { LabelActionsT } from 'src/app/interfaces/labels';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent implements OnInit {
+  @ViewChild("modalContainer ") modalContainer !: ElementRef<HTMLInputElement>
+  @ViewChild("modal") modal !: ElementRef<HTMLInputElement>
+  @ViewChild("labelInput") labelInput !: ElementRef<HTMLInputElement>
+  @ViewChild("labelError") labelError !: ElementRef<HTMLInputElement>
 
   constructor(public Shared: SharedService, public router: Router) { }
+
+  // ? modal ----------------------------------------------------------
+  openModal() {
+    this.modalContainer.nativeElement.style.display = 'block';
+    document.addEventListener('mousedown', this.mouseDownEvent)
+  }
+  hideModal() {
+    this.modalContainer.nativeElement.style.display = 'none'
+    document.removeEventListener('mousedown', this.mouseDownEvent)
+  }
+  mouseDownEvent = (event: Event) => {
+    let modalEl = this.modal.nativeElement
+    if (!(modalEl as any).contains(event.target)) {
+      this.hideModal()
+    }
+  }
+
+  // ? labels ----------------------------------------------------
+
+  addLabel(el: HTMLInputElement) {
+    if (!el) return
+    this.Shared.label.db.add({ name: el.value })
+      .then(() => { this.labelError.nativeElement.hidden = true; el.value = ''; el.focus() })
+      .catch(x => { if (x.name === "ConstraintError") this.labelError.nativeElement.hidden = false; el.focus() })
+  }
+
+  editLabel(id: number) {
+    this.Shared.label.id = id
+    let actions: LabelActionsT = {
+      delete: () => {
+        this.Shared.label.db.delete()
+        this.Shared.label.db.updateAllLabels('')
+      },
+      update: (value: string) => {
+        this.Shared.label.db.update({ name: value })
+        this.Shared.label.db.updateAllLabels(value)
+      }
+    }
+    return actions
+  }
+
 
   collapseSideBar() {
     document.querySelector('[sideBar]')?.classList.toggle('close')
   }
+
 
   ngOnInit(): void {
     this.Shared.closeSideBar.subscribe(x => { if (x) this.collapseSideBar() })
